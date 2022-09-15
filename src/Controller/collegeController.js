063a8e456eefc4250f0a053f428f2c428740d75d
Shailabh <shaisri681@gmail.com>
2022-09-15 12:11:04 +0530

    Ima

--- src/Controller/collegeController.js
+++ src/Controller/collegeController.js
@@ -1,44 +1,44 @@
 const collegeModel = require("../Model/collegeModel")
-const {isString,isNotEmpty,isWrong,isValidCollege,isValidLink}=require("../validators/validator")
-const internModel= require("../Model/internModel")
+const { isString, isNotEmpty, isWrong, isValidCollege, isValidLink } = require("../validators/validator")
+const internModel = require("../Model/internModel")
 
-const createCollege = async function (req,res){
-    try{
-        let data=req.body
+const createCollege = async function (req, res) {
+    try {
+        let data = req.body
 
-        if(Object.keys(data).length==0)return res.status(400).send({status: false,message: "Please provide some data."})
-        let {name, fullName, logoLink}=data
-        
-       
+        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "Please provide some data." })
+        let { name, fullName, logoLink } = data
 
 
-        if(!name)return res.status(400).send({status: false,message: "Please provide name."})
-        if(!isString(name))return res.status(400).send({status: false,message: "Name must be in string."})
-        if(!isNotEmpty(name))return res.status(400).send({status: false,message: "Please provide some data in name."})
-        if(!isWrong(name))return res.status(400).send({status: false,message: "Name must be in alphabets."})
+
+
+        if (!name) return res.status(400).send({ status: false, message: "Please provide name." })
+        if (!isString(name)) return res.status(400).send({ status: false, message: "Name must be in string." })
+        if (!isNotEmpty(name)) return res.status(400).send({ status: false, message: "Please provide some data in name." })
+        if (!isWrong(name)) return res.status(400).send({ status: false, message: "Name must be in alphabets." })
 
         const collegeAlreadyExist = await collegeModel.findOne({ name: name })
         if (collegeAlreadyExist) return res.status(407).send({ msg: `college already Exists`, status: false })
 
-        
-
-        if(!fullName)return res.status(400).send({status: false,message: "Please provide fullName."})
-        if(!isString(fullName))return res.status(400).send({status: false,message: "fullName must be in string."})
-        if(!isNotEmpty(fullName))return res.status(400).send({status: false,message: "Please provide some data in fullName."})
-        if(!isValidCollege(fullName))return res.status(400).send({status: false,message: "fullName "})
-
-        if(!logoLink)return res.status(400).send({status: false,message: "Please provide logo link."})
-        if(!isString(logoLink))return res.status(400).send({status: false,message: "logo link must be in string."})
-        if(!isNotEmpty(logoLink))return res.status(400).send({status: false,message: "Please provide some data in logo link."})
-        if(!isValidLink(logoLink))return res.status(400).send({status: false,message: "Please provide a correct link"})
 
 
+        if (!fullName) return res.status(400).send({ status: false, message: "Please provide fullName." })
+        if (!isString(fullName)) return res.status(400).send({ status: false, message: "fullName must be in string." })
+        if (!isNotEmpty(fullName)) return res.status(400).send({ status: false, message: "Please provide some data in fullName." })
+        if (!isValidCollege(fullName)) return res.status(400).send({ status: false, message: "fullName " })
+
+        if (!logoLink) return res.status(400).send({ status: false, message: "Please provide logo link." })
+        if (!isString(logoLink)) return res.status(400).send({ status: false, message: "logo link must be in string." })
+        if (!isNotEmpty(logoLink)) return res.status(400).send({ status: false, message: "Please provide some data in logo link." })
+        if (!isValidLink(logoLink)) return res.status(400).send({ status: false, message: "Please provide a correct link" })
 
 
-        let createdData=await collegeModel.create(data)
-        res.status(201).send({ data:createdData, status:true })
-    }catch(err){
-        res.status(500).send({msg:err.message, status:false})
+
+
+        let createdData = await collegeModel.create(data)
+        res.status(201).send({ data: createdData, status: true })
+    } catch (err) {
+        res.status(500).send({ msg: err.message, status: false })
 
     }
 }
@@ -53,13 +53,12 @@ const getCollegeDetails = async function (req, res) {
         if (!collegeData) {
             return res.status(400).send({ status: false, msg: "College Not Found" })
         }
-        const internData = await internModel.find({ collegeId: saveData._id, isDeleted: false }).select({ name: 1, email: 1, mobile: 1 })
-        res.status(200).send({ status: true, data:  saveData, interns: internData  })
+        const internData = await internModel.find({ collegeId: collegeData._id, isDeleted: false }).select({ name: 1, email: 1, mobile: 1 })
+        res.status(200).send({ status: true, data: { name: collegeData.name, fullName: collegeData.fullName, logoLink: collegeData.logoLink, interns: internData } })
     } catch (err) {
         res.status(500).send({ msg: err.message, status: false })
     }
 }
 
-
-module.exports.createCollege=createCollege
-module.exports.getCollegeDetails=getCollegeDetails
+module.exports.createCollege = createCollege
+module.exports.getCollegeDetails = getCollegeDetails
